dadf155c2edac31312425a376c6b489d900d6720
abhijayanoop <abhijayanoop007@gmail.com>
2026-01-25 16:42:01 +0530

    duration change in jwt util

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { env } from '../config/environment';
 
 export const generateAccessToken = (userId: string): string => {
-  return jwt.sign({ id: userId }, env.JWT_SECRET, { expiresIn: '15m' });
+  return jwt.sign({ id: userId }, env.JWT_SECRET, { expiresIn: '24h' });
 };
 
 export const generateRefreshToken = (userId: string): string => {
